e4cab0312720efba61fc977734844091500da60e
pando <ndjman7@gmail.com>
2018-02-19 09:07:30 +0900

    Allowed Host 추가

--- pystargram/settings.py
+++ pystargram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7&q7&k9uwa*r@*j90$5nhn!g%359o^1l9lw*$x@e#omug0^(=&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
